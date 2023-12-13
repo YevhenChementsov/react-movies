c4b46147eb99e6675bc9b5374012898381ca539e
Yevhen Chementsov <yevhenchementsov@gmail.com>
2023-12-13 12:49:34 +0200

    Initial commit

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import App from 'components/App/App';
+import ReactDOM from 'react-dom/client';
+
+import 'modern-normalize/modern-normalize.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
